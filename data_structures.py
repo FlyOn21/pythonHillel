e22a357bfb3a25c16182c2bd69c2a5bad6272437
Zhoholiev Pavel <zhogolevpv@gmail.com>
2021-01-28 23:09:35 +0200

    implimented task 2

--- data_structures.py
+++ data_structures.py
@@ -19,12 +19,29 @@ def smallest_positive_integer(A= [1, 3, 6, 4, 1, 2]):
 
 
 
-# 2)Abinary gapwithin a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
-# For example, number 9 has binary representation1001and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps. The number 32 has binary representation 100000 and has no binary gaps.
+# 2)Abinary gapwithin a positive integer N is any maximal sequence of consecutive zeros that
+# is surrounded by ones at both ends in the binary representation of N.
+# For example, number 9 has binary representation1001and contains a binary gap of length 2.
+# The number 529 has binary representation 1000010001 and contains two binary gaps: one of
+# length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1.
+# The number 15 has binary representation 1111 nd has no binary gaps. The number 32 has binary representation 100000 and has no binary gaps.
 # Write a function:
 # def solution(N)
 # that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
-# For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5. Given N = 32 the function should return 0, because N has binary representation '100000' and thus no binary gaps.
+# For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so
+# its longest binary gap is of length 5. Given N = 32 the function should return 0, because N has binary representation '100000' and thus no binary gaps.
+def longest_binary_gap(N = 32):
+    """Function count binary gap in binary representation integer N"""
+    binary_represent_N = bin(N)[2:]
+    list_char_binary_represent_N = str(binary_represent_N).strip('0').split('1')
+    list_binary_gap = [(len(char)) for char in list_char_binary_represent_N if char != '']
+    if list_binary_gap == []:
+        return 0
+    else:
+        return max(list_binary_gap)
+
+    # for char in str(binary_represent_N):
+    #     print(char)
 
 
 # 3)An array A consisting of N integers is given. Rotation of the array means that each element is shifted right by one index, and the last element of the array is moved to the first place. For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7] (elements are shifted right by one index and 6 is moved to the first place).
@@ -52,4 +69,8 @@ if __name__ == '__main__':
 
     print('Task #1')
     result = smallest_positive_integer()
+    print(result)
+
+    print ('Task #2')
+    result = longest_binary_gap()
     print(result)
