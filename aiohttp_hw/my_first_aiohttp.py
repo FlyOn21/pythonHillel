82a0fe6093ba4cac23a38b7f59ced01daeaa52f9
Zhoholiev Pavel <zhogolevpv@gmail.com>
2021-03-19 14:52:05 +0200

    home work aiohttp version 1.0

--- aiohttp_hw/my_first_aiohttp.py
+++ aiohttp_hw/my_first_aiohttp.py
@@ -8,70 +8,69 @@
 # 2)Создать сервер который мог бы принимать GET запросы на адрес (http://localhost/collect_info)
 #
 # 3) В ответе должна быть агрегирована информация полученная от сторонних ресурсов.
-# url = "https://covid-19-data.p.rapidapi.com/totals"
-#
-# querystring = {"format":"json"}
-#
-# headers = {
-#     'x-rapidapi-key': "2a8ce32613msh502a38a434f6605p17519fjsn2656b605e7f2",
-#     'x-rapidapi-host': "covid-19-data.p.rapidapi.com"
-#     }
-#
-# response = requests.request("GET", url, headers=headers, params=querystring)
-import os
 
+import datetime
+import os
+from config import API_KEY
 import aiohttp_jinja2
 import jinja2
 from aiohttp import web, ClientSession
-import asyncio
-import json
+
+
 
 routes = web.RouteTableDef()
 
 
 @routes.get("/")
 async def main(request: web.Request):
-    # data = await weather(url)
     context = {
-        'username': request.match_info.get("username", ""),
-        'current_date': 'January 27, 2017'
+        'current_date': datetime.date.today()
     }
-    respons = aiohttp_jinja2.render_template("index.html", request,
-                                          context=context)
-    return respons
-    # return web.Response(text = data)
+    response = aiohttp_jinja2.render_template("index.html", request,
+                                              context=context)
+    return response
+
+
 
 @routes.get("/collect_info")
 async def collect_info(request: web.Request):
     async with ClientSession() as session:
-        covid_19_data = await covid_19("https://covid-19-data.p.rapidapi.com/totals",session)
+        covid_19_data = await covid_19("https://covid-19-data.p.rapidapi.com/totals", session)
         weather_data = await weather("https://community-open-weather-map.p.rapidapi.com/weather", session)
+    context = {
+        "weather": weather_data,
+        "covid": covid_19_data,
+        "current_date": datetime.date.today()
+    }
+    response = aiohttp_jinja2.render_template("collect_info.html", request,
+                                              context=context)
+    return response
 
 
-async def covid_19(url,session):
+async def covid_19(url, session):
     params = {"format": "json"}
     headers = {
-        'x-rapidapi-key': "2a8ce32613msh502a38a434f6605p17519fjsn2656b605e7f2",
+        'x-rapidapi-key': API_KEY,
         'x-rapidapi-host': "covid-19-data.p.rapidapi.com"
     }
-    response = await session.get(url, params = params, headers = headers)
-    if response:
-        await asyncio.sleep(1)
+    response = await session.get(url, params=params, headers=headers)
+    if response.status == 200:
         covid_19_info = await response.json()
         print(covid_19_info)
         return covid_19_info[0]
     return False
 
-async def weather(url,session):
-    params= {"q": "Kiev", "lang": "ru", "units": "metric", "mode": "json"}
+
+async def weather(url, session):
+    params = {"q": "Kiev", "lang": "ru", "units": "metric", "mode": "json"}
 
     headers = {
-        'x-rapidapi-key': "2a8ce32613msh502a38a434f6605p17519fjsn2656b605e7f2",
+        'x-rapidapi-key': API_KEY,
         'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com"
     }
-    response = await session.get(url, params = params, headers = headers)
-    if response:
-        await asyncio.sleep(1)
+    response = await session.get(url, params=params, headers=headers)
+
+    if response.status == 200:
         weather_info = await response.json()
         print(weather_info)
         return weather_info
@@ -84,6 +83,5 @@ aiohttp_jinja2.setup(
 )
 app.add_routes(routes)
 
-
 if __name__ == "__main__":
-    web.run_app(app, host= "localhost")
+    web.run_app(app, host="localhost", port=2000)
